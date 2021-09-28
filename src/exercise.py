dc9474ffed3a93c670644fac280f86da6e692249
Jake Lee <jake.lee@fdmgroup.com>
2021-09-28 16:40:11 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    nums = []
+    while True:
+        num = int(input())
+        if (num == -1):
+            break
+        else:
+            nums.append(num)
+    greatest = nums[0]
+    for number in nums:
+        if number > greatest:
+            greatest = number
+    print("The greatest number: " + str(greatest))
 
 if __name__ == '__main__':
     main()
